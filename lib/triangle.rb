c6dc844f51dc13cb388456a2d6d4612818043b07
conner fortner <fortner.conner@yahoo.com>
2020-09-28 20:24:14 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,6 +11,18 @@ class Triangle
     sides = [@length_1, @length_2, @length_3]
   end
   
-  def valid?
-    if @leg
+   def valid?
+    if @length_1 + @length_2 > @length_3 && @length_2 + @length_3 > @length_1 && @length_3 + @length_1 > @length_2
+      return true
+    else
+      return false
+    end
+  end
+  
+  def kind
+    if sides.any? {|side| side <= 0 } || sides.any? {|side| side == nil } || self.valid? == false
+      raise TriangleError
+    elsif @length_1 == @length_2 && @length_2 == @length_3
+      return :equilateral
+    elsif @length_1 == @length_2  
 end
